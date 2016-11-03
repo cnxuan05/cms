47f93b372de0a9686d93514a80181a071939ba57
cnxuan05 <cnxuan05@gmail.com>
2016-11-03 14:36:23 +0800

    新建js调试环境

---
+++ html/numbers.js
@@ -0,0 +1,11 @@
+/**
+ * Created by gbis-954 on 2016/11/3.
+ */
+var i = 0;
+for (i = 0; i <= 10; i++) {
+    if (i == 3) {
+        continue;
+    }
+    document.write("The number is " + i);
+    document.write("<br/>");
+}
