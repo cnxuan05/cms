1b6bf09416247aaecb87915c9a38dd4126517651
cnxuan05 <cnxuan05@gmail.com>
2016-11-06 08:26:03 +0800

    调试JS代码

--- main.py
+++ main.py
@@ -2,9 +2,6 @@ import os, sys
 
 pack_py_path = (os.path.dirname(os.path.abspath(__file__)) + '//py-demo')
 sys.path.insert(0, pack_py_path)
-from a7 import *
+# from a7 import *
 
 # os.system('scrapy runspider py-demo/a6.py -o data/1.json')
-
-
-
